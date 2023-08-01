cd48a0d68101711a23c28b905eedca7d6b232ed9
Roman Orzegowski <roman@mbr-targeting.com>
2023-08-01 14:41:07 +0200

    Add lua lsp

--- lsp.lua
+++ lsp.lua
@@ -2,6 +2,25 @@ local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup{}
 lspconfig.rust_analyzer.setup{}
 
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
