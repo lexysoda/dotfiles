eb4c0c8d76d9124be8bcbc2aba9933efc45b197e
Roman Orzegowski <roman@mbr-targeting.com>
2023-10-11 17:21:07 +0200

    Move vim-notes setting out of after
    Should probably change the plugin to dynamically lookup the dir.

--- nvim/after/plugin/vim-notes.lua
+++ nvim/after/plugin/vim-notes.lua
@@ -1,4 +1,3 @@
-vim.g.vimnotes_dir = '$HOME/notes'
 vim.keymap.set('n', '<Leader>vn', ':NotesFZF', { silent = true })
 vim.keymap.set('n', '<Leader>vd', ':NotesOpenDiary<CR>', { silent = true })
 vim.keymap.set('n', '<Leader>vt', ':NotesOpenTopic ')
