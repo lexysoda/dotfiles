123064fc80f0de446da34092d0a11c0c8b11ed53
Roman Orzegowski <roman@mbr-targeting.com>
2024-03-17 15:23:14 +0100

    Add wezterm config
    In gnu stow format

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+
+return config
