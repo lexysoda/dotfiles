112002bbaba002171a46c3de029d5af111c317f4
Roman Orzegowski <roman@mbr-targeting.com>
2024-07-12 20:24:23 +0200

    Update telekasten settings

--- nvim/.config/nvim/after/plugin/telekasten.lua
+++ nvim/.config/nvim/after/plugin/telekasten.lua
@@ -1,3 +1,20 @@
 require('telekasten').setup({
-  home = vim.fn.expand("~/zettelkasten"),
+  home = vim.fn.expand("~/notes"),
+  dailies = vim.fn.expand("~/notes/diary"),
+  templates = vim.fn.expand("~/notes/templates"),
+
+  template_new_note = vim.fn.expand("~/notes/templates/note.md"),
+  template_new_daily = vim.fn.expand("~/notes/templates/diary.md"),
+  template_handling = 'prefer_new_note',
+
+  image_subdir = "img",
+
+  journal_auto_open = true,
+
+  tag_notation = "yaml-bare",
 })
+
+vim.keymap.set('n', '<leader>zf', require('telekasten').find_notes)
+vim.keymap.set('n', '<leader>zd', require('telekasten').goto_today)
+vim.keymap.set('n', '<leader>zg', require('telekasten').search_notes)
+vim.keymap.set('n', '<leader>zn', require('telekasten').new_note)
