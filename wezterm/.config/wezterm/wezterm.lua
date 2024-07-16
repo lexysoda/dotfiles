e774ab757d94ec4ff2fd8f76bd558b4a7a942012
Roman Orzegowski <roman@mbr-targeting.com>
2024-07-16 12:14:01 +0200

    Replace tmux with somewhat matching wezterm config
    No need for tmux, wezterm can handle all of this

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,55 @@
-local wezterm = require 'wezterm'
-local config = {}
+local wez = require('wezterm')
+local act = wez.action
+local c = {}
 
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+c.color_scheme = 'Gruvbox Dark (Gogh)'
 
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+c.enable_tab_bar = true
+c.use_fancy_tab_bar = false
+c.window_decorations = "RESIZE"
 
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+c.font = wez.font('FiraCode Nerd Font Mono')
 
-return config
+c.window_padding = {
+   left = '0.2cell',
+   right = '0.2cell',
+   top = '0.5cell',
+   bottom = 0,
+}
+
+c.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+c.keys = {
+   {
+      key = 's',
+      mods = 'LEADER|CTRL',
+      action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+   },
+   {
+      key = 'v',
+      mods = 'LEADER|CTRL',
+      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+   },
+   {
+      key = 'h',
+      mods = 'LEADER|CTRL',
+      action = act.ActivatePaneDirection('Left'),
+   },
+   {
+      key = 'l',
+      mods = 'LEADER|CTRL',
+      action = act.ActivatePaneDirection('Right'),
+   },
+   {
+      key = 'k',
+      mods = 'LEADER|CTRL',
+      action = act.ActivatePaneDirection('Up'),
+   },
+   {
+      key = 'j',
+      mods = 'LEADER|CTRL',
+      action = act.ActivatePaneDirection('Down'),
+   },
+}
+
+return c
