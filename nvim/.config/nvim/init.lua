9ab12a6c4cc43155bae2afdeb7b57ca21e1b07b1
Roman Orzegowski <roman@mbr-targeting.com>
2024-08-26 14:20:51 +0200

    Update shiftwidth

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,9 +22,9 @@ vim.o.ignorecase = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 8
-vim.o.shiftwidth = 3
+vim.o.shiftwidth = 2
 vim.o.smartcase = true
-vim.o.tabstop = 3
+vim.o.tabstop = 2
 vim.o.termguicolors = true
 vim.cmd.colorscheme('gruvbox')
 
