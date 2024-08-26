4233f27ec2f0621a14ae90d0282dfb37cda5cbb2
Roman Orzegowski <roman@mbr-targeting.com>
2024-08-26 14:20:22 +0200

    Remove vim-notes
    Using Telekasten now.

--- nvim/.config/nvim/after/plugin/vim-notes.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<Leader>vn', ':NotesFZF', { silent = true })
-vim.keymap.set('n', '<Leader>vd', ':NotesOpenDiary<CR>', { silent = true })
-vim.keymap.set('n', '<Leader>vt', ':NotesOpenTopic ')
