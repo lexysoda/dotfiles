7ad8161df1466587c9296e805650e41af3d35433
Roman Orzegowski <roman@mbr-targeting.com>
2025-05-29 15:02:53 +0200

    Replace mason-lspconfig's setup_handlers with vim.lsp.config
    <https://github.com/mason-org/mason-lspconfig.nvim/releases/tag/v2.0.0>
    <https://github.com/williamboman/nvim-config/blob/mason-v2-example/init.lua>
    `:h vim.lsp.confi()`

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -53,17 +53,13 @@ return {
         "rust_analyzer",
         "gopls",
       },
+      automatic_enable = true,
     })
 
     local cap = require('cmp_nvim_lsp').default_capabilities()
-    require('mason-lspconfig').setup_handlers({
-      function (server_name)
-        require('lspconfig')[server_name].setup({
-          capabilities = cap,
-        })
-      end,
+    vim.lsp.config("*", {
+      capabilities = cap,
     })
-
     require('lazydev').setup()
   end,
 }
