1cbd42773fe0e4767be0a2f2935fd8f110a9e05c
Roman Orzegowski <roman@mbr-targeting.com>
2026-01-03 13:15:01 +0100

    Use git search files if in git repo

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -41,8 +41,14 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it Files' })
+    vim.keymap.set('n', '<leader>sf', function()
+      vim.fn.system('git rev-parse --is-inside-work-tree')
+      if vim.v.shell_error == 0 then
+        builtin.git_files()
+      else
+        builtin.find_files()
+      end
+    end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
