56781ae6e3841c96ca04bfb82d379e2ea6407ec3
Roman Orzegowski <roman@mbr-targeting.com>
2026-01-26 15:12:25 +0100

    Use built-in neovim lsp
    Goodbye mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,9 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
     { 'folke/lazydev.nvim', ft = 'lua' },
   },
   config = function()
-
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*.go',
       callback = function()
@@ -14,62 +11,14 @@ return {
       end
     })
 
-    -- vim.api.nvim_create_autocmd('LspAttach', {
-    --   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-    --   callback = function(event)
-    --     local map = function(keys, func, desc)
-    --       vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    --     end
-    --
-    --     map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    --
-    --     map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    --
-    --     map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    --
-    --     map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    --
-    --     map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    --
-    --     map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    --
-    --     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    --
-    --     map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    --
-    --     map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    --
-    --     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    --     if client and client.server_capabilities.documentHighlightProvider then
-    --       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-    --         buffer = event.buf,
-    --         callback = vim.lsp.buf.document_highlight,
-    --       })
-    --
-    --       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-    --         buffer = event.buf,
-    --         callback = vim.lsp.buf.clear_references,
-    --       })
-    --     end
-    --   end,
-    -- })
-    require('mason').setup({})
-
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        "gopls",
-        "lua_ls",
-        "nil_ls",
-        "rust_analyzer",
-        "basedpyright",
-      },
-      automatic_enable = true,
-    })
-
     local cap = require('cmp_nvim_lsp').default_capabilities()
     vim.lsp.config("*", {
       capabilities = cap,
     })
-    require('lazydev').setup()
+
+    vim.lsp.enable('gopls')
+    vim.lsp.enable('lua_ls')
+    vim.lsp.enable('rust_analyzer')
+    vim.lsp.enable('basedpyright')
   end,
 }
